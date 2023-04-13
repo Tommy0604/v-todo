3410fa5f1ef47cc8680014a1f980cbd2ad8fec61
qiz <904859162@qq.com>
2023-04-13 20:10:03 +0800

    feat: Add menu collapse function
    * Add menu collapse function

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,7 @@
   <a-config-provider :locale="locale">
     <a-layout>
       <div class="column-left">
-        <div class="resize-bar"></div>
-        <div class="resize-line"></div>
-        <div class="resize-save">
-          <a-layout-sider>
-            <left-menu></left-menu>
-          </a-layout-sider>
-        </div>
+        <left-menu></left-menu>
       </div>
       <a-layout class="column-right">
         <a-layout-header>
@@ -74,79 +68,4 @@ const locale = zhCN;
   margin: 0 24px;
 }
 
-.resize-bar {
-  width: 200px;
-  min-width: 62px;
-  max-width: 30vw;
-  height: inherit;
-  resize: horizontal;
-  cursor: ew-resize;
-  cursor: col-resize;
-  opacity: 0;
-  overflow: scroll;
-}
-
-/* resize line */
-.resize-line {
-  position: absolute;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  border-right: 2px solid #eee;
-  border-left: 1px solid #fff;
-  pointer-events: none;
-}
-
-.resize-save {
-  display: flex;
-  position: absolute;
-  top: 0;
-  right: 5px;
-  bottom: 0;
-  left: 0;
-  // padding: 16px;
-  overflow-x: hidden;
-
-  >.ant-layout-sider {
-    flex: auto !important;
-    max-width: inherit !important;
-    min-width: inherit !important;
-    width: 100% !important;
-  }
-
-  .ant-menu {
-    width: 100% !important;
-    border-color: #fff;
-  }
-}
-
-.resize-bar:hover~.resize-line,
-.resize-bar:active~.resize-line {
-  border-left: 1px dashed skyblue;
-}
-
-.resize-bar::-webkit-scrollbar {
-  // width: 200px;
-  height: inherit;
-}
-
-/* Firefox只有下面一小块区域可以拉伸 */
-@supports (-moz-user-select: none) {
-
-  .resize-bar:hover~.resize-line,
-  .resize-bar:active~.resize-line {
-    border-left: 1px solid #bbb;
-  }
-
-  .resize-bar:hover~.resize-line::after,
-  .resize-bar:active~.resize-line::after {
-    content: "";
-    position: absolute;
-    width: 16px;
-    height: 16px;
-    bottom: 0;
-    right: -8px;
-    background-size: 100% 100%;
-  }
-}
 </style>
--- src/components/menu/menu.vue
+++ src/components/menu/menu.vue
@@ -1,43 +1,49 @@
 <template>
-  <a-menu id="dddddd" :openKeys="openKeys" :selectedKeys="[$route.path]" mode="inline" @click="handleClick">
-    <!-- mode="vertical" -->
-    <!-- 展开菜单按按钮 -->
-    <!-- <div>
-      <a-button
-        type="primary"
-        style="margin-bottom: 16px"
-        @click="toggleCollapsed"
+  <div id="resizeBar" class="resize-bar"></div>
+  <div class="resize-line"></div>
+  <div class="resize-save">
+    <a-layout-sider :collapsed="collapsed" :trigger="null" collapsible>
+      <a-menu
+        id="menu"
+        :openKeys="state.openKeys"
+        :selectedKeys="[$route.path]"
+        mode="inline"
+        @click="handleClick"
       >
-        <MenuUnfoldOutlined v-if="collapsed" />
-        <MenuFoldOutlined v-else />
-      </a-button>
-    </div> -->
-    <a-menu-item key="/myday">
-      <template #icon>
-        <!-- <smile-outlined :style="{fontSize: '18px'}"/> -->
-        <icon-font type="icon-sun" :style="{ 'font-size': '2rem' }" />
-      </template>
-      <router-link to="/myday">我的一天</router-link>
-    </a-menu-item>
-    <a-menu-item key="/plans">
-      <template #icon>
-        <icon-font type="icon-calendar" :style="{ 'font-size': '2rem' }" />
-      </template>
-      <router-link to="/plans">计划内</router-link>
-    </a-menu-item>
-    <a-menu-item key="/completed">
-      <template #icon>
-        <icon-font type="icon-completed" :style="{ 'font-size': '2rem' }" />
-      </template>
-      <router-link to="/completed">已完成</router-link>
-    </a-menu-item>
-    <a-menu-item key="/all">
-      <template #icon>
-        <icon-font type="icon-all" :style="{ 'font-size': '2rem' }" />
-      </template>
-      <router-link to="/all">全部</router-link>
-    </a-menu-item>
-  </a-menu>
+        <a-menu-item key="/myday">
+          <template #icon>
+            <icon-font type="icon-sun" :style="{ 'font-size': '2rem' }" />
+          </template>
+          <router-link to="/myday">我的一天</router-link>
+        </a-menu-item>
+        <a-menu-item key="/plans">
+          <template #icon>
+            <icon-font type="icon-calendar" :style="{ 'font-size': '2rem' }" />
+          </template>
+          <router-link to="/plans">计划内</router-link>
+        </a-menu-item>
+        <a-menu-item key="/completed">
+          <template #icon>
+            <icon-font type="icon-completed" :style="{ 'font-size': '2rem' }" />
+          </template>
+          <router-link to="/completed">已完成</router-link>
+        </a-menu-item>
+        <a-menu-item key="/all">
+          <template #icon>
+            <icon-font type="icon-all" :style="{ 'font-size': '2rem' }" />
+          </template>
+          <router-link to="/all">全部</router-link>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+  </div>
+  <!-- 展开菜单按按钮 -->
+  <div id="collapsed" class="collapsed">
+    <a-button primary @click="toggleCollapsed">
+      <MenuUnfoldOutlined v-if="collapsed" />
+      <MenuFoldOutlined v-else />
+    </a-button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -46,35 +52,62 @@ export default {
 };
 </script>
 <script setup lang="ts">
-import { defineComponent, reactive, ref, watch } from "vue";
-import {
-  SmileOutlined,
-  TableOutlined,
-  CheckCircleOutlined,
-  FireOutlined
-} from "@ant-design/icons-vue";
-import type { MenuProps } from "ant-design-vue";
+import { onMounted, reactive, ref, watch } from "vue";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons-vue";
 import { IconFont } from "../../shared";
 
 let collapsed = ref(false);
-let selectedKeys = ref<string[]>([]);
-let openKeys = ref<string[]>([]);
+let oldBarWidth = ref<number>();
+const state = reactive({
+  openKeys: [],
+  preOpenKeys: [],
+});
+
 const handleClick = (e) => {
   // console.log("click ================> ", e);
 };
-const titleClick = (e: Event) => {
-  // console.log("titleClick", e);
-};
 const toggleCollapsed = () => {
   collapsed.value = !collapsed.value;
-  openKeys.value = collapsed.value ? [] : ["sub1"];
+  state.openKeys = collapsed.value ? [] : state.preOpenKeys;
+
+  let eleResizeBar = document.getElementById("resizeBar");
+
+  if (eleResizeBar) {
+    eleResizeBar.style.width = !collapsed.value
+      ? `${oldBarWidth.value}px`
+      : "62px";
+  }
 };
 watch(
-  () => openKeys,
-  (val) => {
+  () => state.openKeys,
+  (_val, oldVal) => {
+    state.preOpenKeys = oldVal;
   }
 );
 
+onMounted(() => {
+  let eleResizeBar = document.querySelector("#resizeBar");
+  let eleCollapsed = document.getElementById("collapsed");
+
+  function handleBarWidthChange() {
+    requestAnimationFrame(() => {
+      if (eleCollapsed && eleResizeBar) {
+        const leftOffset = eleResizeBar.clientWidth;
+        // Record the menu width when the menu is put away
+        !collapsed.value && (oldBarWidth.value = leftOffset + 16);
+
+        eleCollapsed.style.left = `${leftOffset}px`;
+      }
+    });
+  }
+  const observer = new ResizeObserver((entries) => {
+    for (let entry of entries) {
+      handleBarWidthChange();
+    }
+  });
+
+  eleResizeBar && observer.observe(eleResizeBar);
+});
 </script>
 
 <style lang="scss" scoped>
@@ -91,7 +124,100 @@ watch(
   height: 100%;
 }
 
-.icon-font {
-  font-size: 2rem;
+::v-deep() {
+  .ant-menu.ant-menu-inline-collapsed > .ant-menu-item {
+    padding: 0 calc(50% - 10px);
+  }
+}
+
+.collapsed {
+  position: absolute;
+  top: calc(50% - 15px);
+  transition: top 1s ease-in-out;
+  > .ant-btn {
+    padding: 4px 8px;
+    border-radius: 50%;
+    border: 0;
+    box-shadow: -10px 0px 10px rgba(255, 255, 255, 1),
+      4px 0px 17px rgb(0, 0, 0, 15%);
+  }
+}
+
+.resize-bar {
+  width: 200px;
+  min-width: 62px;
+  max-width: 30vw;
+  height: inherit;
+  resize: horizontal;
+  cursor: ew-resize;
+  cursor: col-resize;
+  opacity: 0;
+  overflow: scroll;
+  transition: width 0.6s, min-width 1s ease-in-out, max-width 1s ease-in-out;
+}
+
+@media only screen and (min-width: 992px) {
+  .resize-bar {
+    min-width: 66px;
+    max-width: 30vw;
+  }
+}
+
+.resize-line {
+  position: absolute;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  pointer-events: none;
+}
+
+.resize-save {
+  display: flex;
+  position: absolute;
+  top: 0;
+  right: 2px;
+  bottom: 0;
+  left: 0;
+  overflow-x: hidden;
+
+  > .ant-layout-sider {
+    flex: auto !important;
+    max-width: inherit !important;
+    min-width: inherit !important;
+    width: 100% !important;
+  }
+
+  .ant-menu {
+    width: 100% !important;
+    border-color: #fff;
+  }
+}
+
+.resize-bar:hover ~ .resize-line,
+.resize-bar:active ~ .resize-line {
+  border-left: 1px dashed var(--ant-primary-color);
+}
+
+.resize-bar::-webkit-scrollbar {
+  height: inherit;
+}
+
+/* Firefox has only the following small area to stretch */
+@supports (-moz-user-select: none) {
+  .resize-bar:hover ~ .resize-line,
+  .resize-bar:active ~ .resize-line {
+    border-left: 1px solid #bbb;
+  }
+
+  .resize-bar:hover ~ .resize-line::after,
+  .resize-bar:active ~ .resize-line::after {
+    content: "";
+    position: absolute;
+    width: 16px;
+    height: 16px;
+    bottom: 0;
+    right: -8px;
+    background-size: 100% 100%;
+  }
 }
 </style>
